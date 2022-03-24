b9e0bccb0f0cf5f033f401f2233b02bf087d02dc
AlexanderLJX <83382087+AlexanderLJX@users.noreply.github.com>
2022-03-24 15:47:10 +0800

    Delete riotwatcher directory

--- riotwatcher/__version__.py
+++
@@ -1,3 +0,0 @@
-__version__ = "3.2.0"
-__author__ = "pseudonym117"
-__title__ = "RiotWatcher"
--- riotwatcher/_apis/league_of_legends/urls/LolStatusApiV4Urls.py
+++
@@ -1,5 +0,0 @@
-from .LeagueEndpoint import LeagueEndpoint
-
-
-class LolStatusApiV4Urls:
-    platform_data = LeagueEndpoint("/status/v4/platform-data")
